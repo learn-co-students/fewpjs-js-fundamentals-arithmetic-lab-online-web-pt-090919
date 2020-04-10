6b07e22ce4ebce0fbcf8cd7d8a47330dd57c71f4
Lauren Dove <elledoveinc@gmail.com>
2020-04-10 08:05:35 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31
+const num2 = 2
 
+let multiply = num1 * num2
+
+const random = Math.random()+1;
 // Write your code here
+
+const mod = 16 % 6
+
+const max = Math.max(13, 19, 0, 20, 16);
